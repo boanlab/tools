e96014e5bf71341199c4c44905e38024a790dc59
Jaehyun Nam <jaehyun.nam@dankook.ac.kr>
2023-01-19 17:47:51 +0000

    update scripts
    Signed-off-by: Jaehyun Nam <jaehyun.nam@dankook.ac.kr>

--- install-chrome.sh
+++ install-chrome.sh
@@ -5,5 +5,5 @@ sudo apt-get update
 
 # install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get -y install ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
---
+++ install-hangul.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# update repo
+sudo apt-get update
+
+# install language pack
+sudo apt-get -y install language-pack-ko
+
+# install nanum font
+sudo apt-get -y install fonts-nanum fonts-nanum-coding fonts-nanum-extra
