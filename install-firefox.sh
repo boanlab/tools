5a22008078e22db1a38690f9c7c5d6aef4ff89a7
Jaehyun Nam <jaehyun.nam@dankook.ac.kr>
2023-01-19 15:22:39 +0000

    update scripts
    Signed-off-by: Jaehyun Nam <jaehyun.nam@dankook.ac.kr>

---
+++ install-firefox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# update repo
+sudo apt-get update
+
+# install firefox
+sudo apt-get -y install firefox
---
+++ install-ubuntu-desktop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# update repo
+sudo apt-get update
+
+# install ubuntu desktop
+sudo apt-get -y install --no-install-recommends ubuntu-desktop
