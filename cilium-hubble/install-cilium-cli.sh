151d4b31b3002b9a6acb4ad05a9a504be5338037
Jaehyun Nam <jaehyun.nam@dankook.ac.kr>
2023-08-04 18:16:09 +0000

    update scripts
    Signed-off-by: Jaehyun Nam <jaehyun.nam@dankook.ac.kr>

---
+++ cilium-hubble/install-cilium-cli.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install cilium cli
+curl -LO https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz
+sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+rm cilium-linux-amd64.tar.gz
+
