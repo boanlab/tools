ba1981671a6c158fd11b4439823c59868548ea7c
Jaehyun Nam <jaehyun.nam@dankook.ac.kr>
2023-09-29 11:06:04 +0000

    update scripts
    Signed-off-by: Jaehyun Nam <jaehyun.nam@dankook.ac.kr>

---
+++ install-kvm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# install kvm and dependencies
+sudo apt-get install -y curl xml2 bridge-utils cpu-checker libguestfs-tools libvirt-clients libvirt-daemon-system libvirt-dev qemu-kvm virt-manager
